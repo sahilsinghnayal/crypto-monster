825eeb96ec2fa11ad7239898ae797191da679c95
sahilsinghnayal <sahilsinghnayal@gmail.com>
2022-05-19 12:36:09 +0530

    carousel added

--- src/Components/Banner/Caurosel.js
+++ src/Components/Banner/Caurosel.js
@@ -1,17 +1,90 @@
-import { makeStyles } from '@material-ui/core'
-import React from 'react'
-const useStyle=makeStyles((theme)=>({
-    caurosel:{
-        display:"flex",
-        height:"50%",
-        
-    },
-})); 
+import { makeStyles } from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
+import { TrendingCoins } from "../../config/Api";
+import { CryptoState } from "../../CryptoContext";
+const useStyle = makeStyles(() => ({
+  caurosel: {
+    display: "flex",
+    height: "50%",
+  },
+  carouselItem: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    cursor: "pointer",
+    textTransform: "uppercase",
+    color: "white",
+  },
+}));
+export function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 function Caurosel() {
-    const classes= useStyle();
+  const classes = useStyle();
+  const [trending, setTrending] = useState([]);
+  const { currency, symbol } = CryptoState();
+  const fetchTrendingcoin = async () => {
+    const { data } = await axios.get(TrendingCoins(currency));
+    setTrending(data);
+  };
+  console.log(trending);
+  useEffect(() => {
+    fetchTrendingcoin();
+  }, [currency]);
+
+  const items = trending.map((coin) => {
+    let profit = coin?.price_change_percentage_24h >= 0;
+    return (
+      <Link className={classes.carouselItem} to={`/coins/${coin.id}`}>
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height="80"
+          style={{ marginBottom: 10 }}
+        />
+        <span>{coin.symbol}</span>
+        &nbsp;
+        <span
+          style={{
+            color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+            fontWeight: 500,
+          }}
+        >
+          {profit && "+"} {coin?.price_change_percentage_24h?.toFixed(2)}%
+        </span>
+        <span style={{ fontSize: 22, fontWeight: 500 }}>
+          {symbol} {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
+      </Link>
+    );
+  });
+  const responsive = {
+    0: {
+      items: 2,
+    },
+    512: {
+      items: 4,
+    },
+  };
+  // responsive if it is 0 px then it will show 2 item and if 512 px it will show 4 item
   return (
-    <div className={classes.caurosel}>Caurosel</div>
-  )
+    <div className={classes.caurosel}>
+      <AliceCarousel
+        mouseTracking
+        infinite
+        autoPlayInterval={1000}
+        animationDuration={1500}
+        disableDotsControls
+        disableButtonsControls
+        responsive={responsive}
+        autoPlay
+        items={items}
+      />
+    </div>
+  );
 }
 
-export default Caurosel
+export default Caurosel;
