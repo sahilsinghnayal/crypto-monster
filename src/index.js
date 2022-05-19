3b6fae1fa02ca45ea7e4b946a4dfbff24864639e
sahilsinghnayal <sahilsinghnayal@gmail.com>
2022-05-19 12:36:58 +0530

    banner cr added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import CryptoContext from "./CryptoContext"
+import CryptoContext from "./CryptoContext";
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 
 ReactDOM.render(
